0020b2b16be39ddc0663144420d4c1de60b01a9a
Joe Chasinga <jo.chasinga@gmail.com>
2018-12-06 22:24:08 -0800

    Refactor io

--- ch2/io/src/main.rs
+++ ch2/io/src/main.rs
@@ -2,11 +2,12 @@ use std::fs::OpenOptions;
 use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
-    let mut file = OpenOptions::new()
-                .read(true)
-                .write(true)
-                .create(true)
-                .open("/tmp/file")?;
-    file.write_all(b"hello world\n")?;
+    OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open("/tmp/file")
+        .unwrap()
+        .write_all(b"hello world\n")?;
     Ok(())
 }
