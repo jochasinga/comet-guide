8729f8c05361dd18aaec5262a78725be88f50356
Joe Chasinga <jo.chasinga@gmail.com>
2018-12-10 03:12:52 -0800

    Use c_int instead of isize 🔢
    Also use explicit type annotation even when reading from a deref pointer

--- ch2/mem/src/main.rs
+++ ch2/mem/src/main.rs
@@ -1,18 +1,19 @@
 use std::process;
 use std::{thread, time};
 use std::alloc::{alloc, Layout};
+use std::os::raw::c_int;
 
 fn main() {
     // This is unsafe code
     unsafe {
-        let layout = Layout::new::<isize>();
+        let layout = Layout::new::<c_int>();
         let p = alloc(layout);
         println!("({}) address pointed to by p: {:p}", process::id(), p);
-        *(p as *mut isize) = 0;
+        *(p as *mut c_int) = 0;
         loop {
             thread::sleep(time::Duration::from_millis(1000));
-            *(p as *mut isize) += 1;
-            println!("({}) p: {}", process::id(), *p);
+            *(p as *mut c_int) += 1;
+            println!("({}) p: {}", process::id(), *(p as *mut c_int));
         }
     }
 }
